bd52f11f9ee55f019716390982824f3aeb8d3880
ayakovlev <andrew.yakovlev63@gmail.com>
2026-03-01 19:59:58 +0400

    #0001 Remove Research from version control; Add interview-prep to the version control

---
+++ interview-prep/src/test/java/com/ayakovlev/interviewprep/InterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayakovlev.interviewprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
