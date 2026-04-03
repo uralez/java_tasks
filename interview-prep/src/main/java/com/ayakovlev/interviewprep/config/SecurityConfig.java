cc83c30262ae3795a1478d38d04bbcaa9a32c327
ayakovlev <andrew.yakovlev63@gmail.com>
2026-04-04 00:02:22 +0400

    #21 refactor: extract head and topbar fragments, update all pages

--- interview-prep/src/main/java/com/ayakovlev/interviewprep/config/SecurityConfig.java
+++ interview-prep/src/main/java/com/ayakovlev/interviewprep/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth // фильтр проверки прав доступа
                         .requestMatchers(
                                 "/login",
+                                "/login/**",
                                 "/register",
                                 "/demo/login", // Разрешить доступ к /demo/login без аутентификации
                                 "/css/**",
